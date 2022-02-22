8baa8932ac6cd5368df4956c7d8285cbc3e1ab12
Nicholas Payne <nic.payne@protonmail.com>
2022-02-21 20:48:19 -0600

    write app

---
+++ app/server.py
@@ -0,0 +1,11 @@
+"""http server for hosting notes"""
+
+import http.server
+import socketserver
+
+PORT = 8080
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
