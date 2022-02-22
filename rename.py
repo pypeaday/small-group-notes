c3de15a444b93828bc7cd6dc437ed32de4cf5efa
Nicholas Payne <nic.payne@protonmail.com>
2022-02-21 20:33:58 -0600

    add docstring

--- rename.py
+++ rename.py
@@ -5,6 +5,19 @@ import os
 
 
 def get_new_name(filepath: Path) -> Path:
+    """get_new_name.
+
+    Args:
+        filepath (Path): filepath to exported tiddler
+
+    Returns:
+        new_filename (Path): target filepath for rename
+
+    Examples:
+    >>> filepath = Path("/foo/bar/10_ Title subtitle.html")
+    >>> new_filepath = get_new_name(filepath)
+    ... Path("/foo/bar/10: Title subtitle.html")
+    """
 
     try:
         digi, title = filepath.name.split("_")
